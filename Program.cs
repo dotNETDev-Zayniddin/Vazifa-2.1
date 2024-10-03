c404adf38f2651c1b4c87eb16186b81082e22a01
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-03 21:52:24 +0500

    Vazifa: Project Upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace Vazifa{
+    class Program{
+        static void Main(string[] args){
+            System.Console.Write("Birinchi sonni kiriting: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ikkinchi sonni kiriting: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.Write("Amalni kiriting (+, -, *, /.): ");
+            char amal = Convert.ToChar(Console.ReadLine());
+
+            bool add = amal == '+';
+            bool subtract = amal == '-';
+            bool multiply = amal == '*';
+            bool divide = amal == '/';
+
+            int result = ((firstNumber+secondNumber)*Convert.ToInt32(add) + (firstNumber-secondNumber)*Convert.ToInt32(subtract)+(firstNumber*secondNumber)*Convert.ToInt32(multiply)+(firstNumber/secondNumber)*Convert.ToInt32(divide));
+            Console.WriteLine($"Natija: {result}");
+
+
+
+        }
+    }
+}
