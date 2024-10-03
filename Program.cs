0c04e573d68b695679c5b72983e977e1810cb319
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-03 22:01:57 +0500

    Vazifa: Upgrade version

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ namespace Vazifa{
             System.Console.Write("Birinchi sonni kiriting: ");
             int firstNumber = Convert.ToInt32(Console.ReadLine());
 
+             System.Console.Write("Amalni kiriting (+, -, *, /.): ");
+            char amal = Convert.ToChar(Console.ReadLine());
+
             Console.Write("Ikkinchi sonni kiriting: ");
             int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-            System.Console.Write("Amalni kiriting (+, -, *, /.): ");
-            char amal = Convert.ToChar(Console.ReadLine());
+          
 
             bool add = amal == '+';
             bool subtract = amal == '-';
@@ -18,6 +20,7 @@ namespace Vazifa{
             bool divide = amal == '/';
 
             int result = ((firstNumber+secondNumber)*Convert.ToInt32(add) + (firstNumber-secondNumber)*Convert.ToInt32(subtract)+(firstNumber*secondNumber)*Convert.ToInt32(multiply)+(firstNumber/secondNumber)*Convert.ToInt32(divide));
+            //Agar bool amal o'zgaruvchisi qaysidir to'rt amaldan  birini olsa 
             Console.WriteLine($"Natija: {result}");
 
 
